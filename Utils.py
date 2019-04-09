7fb5a0945359d11e7ec2cf28a6c2d53560835375
abishalom <google-dl-platform@googlegroups.com>
2019-04-09 04:41:46 +0000

    Updated and created DCGAN. Still not working well

--- Utils.py
+++ Utils.py
@@ -74,3 +74,15 @@ def loss_plot(hist, path = 'Train_hist.png', model_name = ''):
     plt.savefig(path)
 
     plt.close()
+    
+def initialize_weights(net):
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.ConvTranspose2d):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.Linear):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
